aba9b89a23b8f347709ae9616f798fbfa683d52f
Unknown <11601037@student.pxl.be>
2018-10-23 13:51:24 +0200

    Add connection check with dialog at startup of application

--- app/src/main/java/be/pxl/student/t_rail/LaunchActivity.java
+++ app/src/main/java/be/pxl/student/t_rail/LaunchActivity.java
@@ -1,8 +1,17 @@
 package be.pxl.student.t_rail;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+import be.pxl.student.t_rail.domainClasses.ClickEvent;
+import be.pxl.student.t_rail.interfaces.IEvent;
 import be.pxl.student.t_rail.tasks.StationsHttpTask;
 
 public class LaunchActivity extends AppCompatActivity {
@@ -11,9 +20,43 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        checkConnection();
+    }
 
-        StationsHttpTask task = new StationsHttpTask(LaunchActivity.this);
-        task.execute();
+
+    private boolean hasActiveInternetConnection(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = manager.getActiveNetworkInfo();
+        if(activeNetworkInfo == null || !activeNetworkInfo.isConnected()){
+            return false;
+        }
+        return true;
+    }
+
+    private void checkConnection(){
+        DialogInterface.OnClickListener negativeEvent = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                checkConnection();
+            }
+        };
+        DialogInterface.OnClickListener positiveEvent = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                LaunchActivity.this.finishAffinity();
+            }
+        };
+
+        if(hasActiveInternetConnection()){
+            StationsHttpTask task = new StationsHttpTask(LaunchActivity.this);
+            task.execute();
+        }
+
+        else{
+            new AlertDialog.Builder(LaunchActivity.this).setTitle("Netwerk error").setMessage("Deze applicatie heeft een mobiele verbinding nodig.")
+                    .setPositiveButton("Afsluiten",positiveEvent).setNegativeButton("Probeer Opnieuw",negativeEvent).setCancelable(false).show();
+        }
 
     }
 }
